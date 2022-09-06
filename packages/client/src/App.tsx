e6dbee3bc97dc6429cb1eaeee69e68b11d0a3174
MassimoTambu <massimo.tamburini.work@gmail.com>
2022-09-06 20:28:36 +0200

    feat(client): add appbar and fetch data from server

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,42 @@
+import { ThemeProvider } from '@mui/material/styles';
+import { useEffect, useState } from 'react';
 import './App.css';
+import BlastAppBar from './components/app_bar';
+import theme from './theme';
+import { GameDataResponse } from 'shared/models/game_data_response';
 
 function App() {
-  return <div className="App"></div>;
+  const [gameData, setGameData] = useState<GameDataResponse | null>(null);
+
+  useEffect(() => {
+    async function getStatistics() {
+      const res = await fetch('http://127.0.0.1:4000/statistics', {
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+
+      const data = (await res.json()) as GameDataResponse;
+      console.log(
+        'Watch out for React.StrictMode: it will render components twice'
+      );
+      console.log(data);
+
+      setGameData(data);
+    }
+
+    getStatistics();
+  }, []);
+
+  return (
+    <div className="App">
+      {gameData && (
+        <ThemeProvider theme={theme}>
+          <BlastAppBar />
+        </ThemeProvider>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ packages/client/src/theme.tsx
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#000037',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    info: {
+      main: '#4173ff',
+    },
+  },
+});
+
+export default theme;
